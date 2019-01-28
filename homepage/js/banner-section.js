c27b3aeea4e5d9ec91c9f8e975d28a822408e2ac
James Russo <1jlrusso@gmail.com>
2019-01-27 17:33:27 -0800

    configure module as object and update scroll path for btn click

--- homepage/js/banner-section.js
+++ homepage/js/banner-section.js
@@ -1,15 +1,23 @@
-(function(){
-  cacheAndBindEventHandlers();
-  function cacheAndBindEventHandlers(){
-    const sectionOffset = document.getElementById("user-location-section").offsetTop - 90;
-    const bannerBtns = document.getElementsByClassName("banner-btn");
-    [...bannerBtns].forEach(btn => btn.addEventListener("click", scrollToMapSection.bind(null, sectionOffset)));
-  }
-  function scrollToMapSection(sectionOffset){
+const bannerSection = {
+  init: function(){
+    this.cacheDOM();
+    this.bindEvents();
+  },
+  cacheDOM: function(){
+    this.bannerBtns = document.getElementsByClassName("banner-btn");
+  },
+  bindEvents: function(){
+    this.bannerBtns[0].addEventListener('click', this.scrollToSection.bind(this, 'food-area'));
+    this.bannerBtns[1].addEventListener('click', this.scrollToSection.bind(this, 'user-map-area'));
+  },
+  scrollToSection: function(section){
+    const sectionOffset = document.getElementById(section).offsetTop - 90;
     window.scroll({
       top: sectionOffset,
       left: 0,
       behavior: "smooth"
     });
   }
-})();
+}
+
+module.exports = bannerSection;
