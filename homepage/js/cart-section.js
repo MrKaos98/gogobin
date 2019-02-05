e74116c9159c5ad9f89d31ca1eb2d10ddf72cacc
James Russo <1jlrusso@gmail.com>
2019-02-04 16:38:48 -0800

    require store and add methods to show and hide cart, load, remove cart items

--- homepage/js/cart-section.js
+++ homepage/js/cart-section.js
@@ -1,32 +1,36 @@
-const cartModalObj = {
+const { removeCartItem } = require('../../store/action-creators/action-creators');
+const store = require('../../store/store-index');
+
+const cartSection = {
   init: function(){
     this.cacheDom();
     this.bindEvents();
-    this.foodItemIds = "";
-    this.itemNames = "";
-    if(headerObj.editInProgressVal == "yes" || headerObj.sameOrderInProgressVal == "yes"){
-      this.addCartImgSrcsToArray();
-    }
   },
   cacheDom: function(){
-    this.cartImgSrcArr = [];
+    this.shoppingCart = document.getElementById("shopping-cart");
+    this.cartWindow = document.getElementById("cart-window");
+    this.cartWrap = document.getElementById("cart-wrap");
+    this.closeCartBtn = document.getElementById("close-cart-btn");
     this.placeOrderBtn = document.getElementById("place-order-btn");
     this.noGroceriesAdded = document.getElementById("no-groceries-added");
     this.cartItemsWrap = document.getElementById("cart-items-wrap");
     this.cartItemTemplate = document.getElementsByClassName("cart-item")[0];
     this.cartItemHeading = document.getElementsByClassName("cart-item-heading")[0];
     this.cartImage = document.getElementsByClassName("cart-image")[0];
+    this.cartBadge = document.getElementById("cart-badge");
     this.editBtnTemplate = document.getElementsByClassName("edit-item-btn")[0];
     this.removeBtnTemplate = document.getElementsByClassName("remove-item-btn")[0];
     this.locationTimeContainer = document.getElementById("location-and-time-container");
-    this.locationTimeChildren = document.querySelectorAll("#location-and-time-container > div");
     this.cartLocation = document.getElementById("cart-location");
     this.cartStoreName = document.getElementById("cart-store-name");
     this.cartStoreAddress = document.getElementById("cart-store-address");
     this.cartDeliveryTime = document.getElementById("cart-delivery-time");
   },
   bindEvents: function(){
+    this.shoppingCart.addEventListener('click', this.showCartHandler.bind(this));
     this.placeOrderBtn.addEventListener("click", this.placeOrderHandler.bind(this));
+    this.cartWindow.addEventListener('click', this.hideCartHandler.bind(this));
+    this.closeCartBtn.addEventListener('click', this.hideCartHandler.bind(this));
     document.addEventListener("click", function(e){
       if(e.target.getAttribute("class") === "edit-item-btn"){
         this.editItemHandler(e);
@@ -35,114 +39,84 @@ const cartModalObj = {
       }
     }.bind(this));
   },
-  cacheItemDetails: function(index){
-    this.placeOrderBtn.style.display = "block";
-    this.noGroceriesAdded.style.display = "none";
-    this.setNewCartItem(index);
+  showCartHandler: function(){
+    this.cartWindow.style.display = "block";
+    this.cartWrap.style.display = "block";
+    this.closeCartBtn.style.display = "block";
+    this.loadCartItems();
+    this.loadLocationInfo();
   },
-  setNewCartItem(index){
-    this.cartItemTemplate.setAttribute("id", `item-${index + 1}-wrap`);
-    this.cartImage.setAttribute("src", `../img/image${index + 1}.jpg`);
-    this.editBtnTemplate.setAttribute("data-target", `image${index + 1}`);
-    this.removeBtnTemplate.setAttribute("data-target", `image${index + 1}`);
-    let newCartItem = this.cartItemTemplate.cloneNode(true);
-    this.cartItemsWrap.appendChild(newCartItem);
+  hideCartHandler: function(){
+    this.cartWindow.style.display = "none";
+    this.cartWrap.style.display = "none";
+    this.closeCartBtn.style.display = "none";
+  },
+  loadCartItems: function(){
+    this.cartItemsWrap.innerHTML = "";
+    const cartItemObjs = store.getState().cart.cartItems;
+    if(cartItemObjs.length > 0){
+      this.placeOrderBtn.style.display = "block";
+      this.noGroceriesAdded.style.display = "none";
+      cartItemObjs.forEach(item => {
+        const rowItem = this.cartItemTemplate.cloneNode(true);
+        rowItem.style.display = "block";
+        rowItem.children[0].textContent = item.name;
+        rowItem.children[1].setAttribute('src', item.image);
+        rowItem.children[1].setAttribute('alt', item.name);
+        rowItem.children[2].children[2].textContent = item.quantity;
+        rowItem.children[2].children[3].textContent = `$${item.quantity * 1.99}`;
+        rowItem.children[3].children[1].setAttribute('item-index', item.index);
+        this.cartItemsWrap.appendChild(rowItem);
+      });
+    }
+  },
+  loadLocationInfo: function(){
+    const cartLocation = store.getState().cart.cartLocation;
+    this.locationTimeContainer.children[0].textContent = cartLocation.city;
+    this.locationTimeContainer.children[1].textContent = cartLocation.store;
+    this.locationTimeContainer.children[2].textContent = cartLocation.address;
+    this.locationTimeContainer.children[3].textContent = cartLocation.delivery;
   },
   editItemHandler(e){
     let target = e.target.parentElement.parentElement
     console.log(target);
   },
   removeItemHandler: function(e){
-    let target = e.target.parentElement.parentElement
-    target.parentElement.removeChild(target);
-    headerObj.decrementCartBadgeNum();
-    // let $thisBtn = $(event.target);
-    // let $thisFoodRow = $thisBtn.parents(".cart-row");
-    // let $modalImageData = $thisFoodRow.find("input:image").attr("data");
-    // let $modalImageSrc = $thisFoodRow.find("input:image").attr("src");
-    // let $modalItemName = $thisFoodRow.find("input:image").attr("alt");
-    // let imgSrc = "" + $modalImageSrc;
-    // let imgData = "" + $modalImageData;
-    // let itemName = "" + $modalItemName;
-    // let $nextLineDivider = $(event.target).parents(".cart-row").next(".line-divider");
-    // let $hiddenInput = $("#form-inner").find("input[data='" + imgData + "']");
-    // this.removeCartRowAndInput($thisFoodRow, $nextLineDivider, $hiddenInput);
-    // this.removeImgSrcFromArray($modalImageSrc);
-    // this.removeItemName(itemName);
-    // this.removeFoodItemId(imgData, this.foodItemIds);
+    const itemIndex = e.target.getAttribute('item-index');
+    store.dispatch(removeCartItem(itemIndex));
+    this.hideCartHandler();
+    this.updateCartBadge();
+    this.removeCartItem(e);
   },
-  displayFromCart: function(imgData){
-    itemModalObj.displayItemModal(imgData - 1);
+  updateCartBadge: function(){
+    const cartItemsLength = store.getState().cart.cartItems.length;
+    this.cartBadge.textContent = cartItemsLength;
+    if(cartItemsLength > 0){
+      this.cartBadge.style.display = "block";
+      this.noGroceriesAdded.style.display = "none";
+      this.placeOrderBtn.style.display = "block";
+    } else {
+      this.cartBadge.style.display = "none";
+      this.noGroceriesAdded.style.display = "block";
+      this.placeOrderBtn.style.display = "none";
+    }
   },
-  removeItemName: function(itemName){
-    var tempNames = this.itemNames.replace(itemName + " ", "");
-    this.itemNames = tempNames.replace("  ", " ");
-  },
-  removeFoodItemId(imgData){
-    var tempIds = this.foodItemIds.replace(imgData + " ", "");
-    this.foodItemIds = tempIds.replace("  ", " ");
-  },
-  removeImgSrcFromArray: function(imgSrc){
-    var index = this.cartImgSrcArr.indexOf(imgSrc);
-    this.cartImgSrcArr.splice(index, 1);
+  removeCartItem: function(e){
+    const rowItem = e.target.parentElement.parentElement;
+    rowItem.remove();
   },
   placeOrderHandler: function(){
-    itemModalObj.checkCurrOrders();
-    setTimeout(() => {
-      this.cartImgSrcArr = [];
-      if(itemModalObj.currOrderStatus == "order in progress"){
-        alert("You have an order in progress");
-      } else if(headerObj.editInProgressVal != "yes" && headerObj.sameOrderInProgressVal != "yes"){
-        this.addValuesToCartLocation();
-      } else if (headerObj.editInProgressVal == "yes" || headerObj.sameOrderInProgressVal == "yes"){
-        this.getItemIdsAndNames();
-      }
-      this.setFinalIdsAndNames(this.foodItemIds, this.itemNames);
-      this.hiddenSubmit.click();
-    }, 500);
-  },
-  setFinalIdsAndNames: function(foodIds, itemNames){
-    var foodIdsInput = document.querySelector("input[name='food_ids'");
-    var itemNamesInput = document.querySelector("input[name='item_names'");
-    foodIdsInput.setAttribute("value", foodIds);
-    itemNamesInput.setAttribute("value", itemNames);
-  },
-  addValuesToCartLocation: function(){
-    $("input[name='store_city']").attr("value", mapSectionObj.selectedCity);
-    $("input[name='store_address']").attr("value", mapSectionObj.selectedStoreAddress);
-    $("input[name='store_name']").attr("value", mapSectionObj.selectedStore);
-    $("input[name='delivery_time']").attr("value", mapSectionObj.selectedTime);
-  },
-  addCartImgSrcsToArray: function(){
-    for(let i = 0; i < this.cartImages.length; i++){
-      this.cartImgSrcArr.push(this.cartImages[i].getAttribute("src"));
-    }
-  },
-  checkImgSrcArr: function(imgSrc, index){
-    if(this.cartImgSrcArr.indexOf(imgSrc) > -1){
-      alert("Item in cart. Make changes in cart");
+    if(store.getState().order.orderInProgress && store.getState().order.editInProgress === false){
+      alert('You already have an order in progress');
+    } else if (store.getState().order.editInProgress && store.getState().order.orderInProgress === false){
+      //get all cart item ids and then
+      //this.updateOrderItemIds()
+      //store.dispatch(updateOrderItemIds)
     } else {
-      itemModalObj.addItemDataHandler(index);
-      this.cartImgSrcArr.push(imgSrc);
-      $("body").removeClass("stop-scrolling");
-    }
-  },
-  updateCartLocationAndTime: function(){
-    this.resetLocationTimeChildren();
-    this.locationTimeContainer.style.display = "grid";
-    this.cartLocation.textContent = mapSectionObj.selectedCity.textContent;
-    this.cartStoreName.textContent = mapSectionObj.selectedStore;
-    this.cartStoreAddress.textContent = mapSectionObj.selectedStoreAddress;
-    this.cartDeliveryTime.textContent = mapSectionObj.selectedTime;
-    $("input[name='store_address']").val(mapSectionObj.selectedStoreAddress);
-    $("input[name='store_name']").val(mapSectionObj.selectedStore);
-    $("input[name='store_city']").val(mapSectionObj.selectedCity.textContent);
-    $("input[name='delivery_time']").val(mapSectionObj.selectedTime);  
-  },
-  resetLocationTimeChildren: function(){
-    for(let div of this.locationTimeChildren){
-      div.textContent = "";
+      //get all cart item ids and then
+      //this.updateOrderItemIds()
+      //store.dispatch(updateOrderItemIds)
     }
   }
 }
-cartModalObj.init();
+module.exports = cartSection;
