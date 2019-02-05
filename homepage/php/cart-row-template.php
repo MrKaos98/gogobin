be22e2a4f69b4163ef3ae20e53d2c11086bb5f9e
James Russo <1jlrusso@gmail.com>
2019-02-04 16:32:40 -0800

    make item quantity and total value dynamic

--- homepage/php/cart-row-template.php
+++ homepage/php/cart-row-template.php
@@ -2,12 +2,12 @@
   echo '
     <li class="cart-item" style="display: none">
       <h4 class="cart-item-heading"></h4>
-      <img class="cart-image"/>
+      <img class="cart-image" />
       <p>
         <span class="cart-item-cost">$1.99/lb</span> &nbsp; 
         <i class="fa fa-times"></i>&nbsp; 
-        <span>3</span> &nbsp; = &nbsp; 
-        <span class="cart-item-total">$5.97</span>
+        <span class="cart-item-quantity"></span> &nbsp; = &nbsp; 
+        <span class="cart-item-total"></span>
       </p>
       <div class="cart-item-btns-wrap">
         <button type="button" class="edit-item-btn">Edit Item</button>
