d60b914b8eef6ac7925b8aadb87c549d1106a3cf
James Russo <1jlrusso@gmail.com>
2019-02-04 18:26:03 -0800

    update html tags for call to action and wrap image in figure

--- homepage/php/banner-section.php
+++ homepage/php/banner-section.php
@@ -1,20 +1,19 @@
 <?php
   echo "
     <section id='banner-wrap'>
-      <div id='banner-content'>
+      <div id='cta-wrap'>
         <div>
-          <h1>Food to your door<br/> is just clicks away.</h1>
-          <p id='hero-caption'>With Gogobin, you can have the convenience of having groceries delivered straight to your door without the hassle of conventional grocery shopping</p>
+          <h1>Food to your door is just clicks away</h1>
+          <h4>Get the convenience of having groceries delivered straight to your door without the hassle of conventional grocery shopping.</h4>
           <div id='btn-grid'>
             <button type='button' class='banner-btn'>Place an Order</button>
             <button type='button' class='banner-btn'>Make a Delivery</button>
           </div>
         </div>
-        <img src='../img/grocery-bag.png' alt='Grocery Bag'/ id='hidden-bag'>
       </div>
-      <div id='banner-img-wrap'>
-        <img src='../img/grocery-bag.png' alt='Grocery Bag' id='grid-bag'/>
-      </div>
+      <figure id='banner-img-wrap'>
+        <img src='../img/grocery-bag.png' alt='Grocery Bag'/>
+      </figure>
     </section>
   ";
 ?>
