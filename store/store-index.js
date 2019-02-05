46723a9bb625b2b157c9e187bd1de6849c0322b7
James Russo <1jlrusso@gmail.com>
2019-02-04 16:25:19 -0800

    add order and cart reducers, update login reducer ref

--- store/store-index.js
+++ store/store-index.js
@@ -1,7 +1,11 @@
 const redux = require('redux');
 const loginReducer = require('./reducers/login-reducer');
+const orderReducer = require('./reducers/order-reducer');
+const cartReducer = require('./reducers/cart-reducer');
 const rootReducer = redux.combineReducers({
-  loginReducer
+  login: loginReducer,
+  order: orderReducer,
+  cart: cartReducer
 });
 const store = redux.createStore(rootReducer);
 module.exports = store;
