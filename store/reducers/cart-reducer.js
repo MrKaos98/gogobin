2bb6eba7b12d96b5e64927c8088e0d4b240c77de
James Russo <1jlrusso@gmail.com>
2019-02-07 21:36:49 -0800

    add locationAndTime object and values to show them and cart calculations

--- store/reducers/cart-reducer.js
+++ store/reducers/cart-reducer.js
@@ -1,31 +1,38 @@
 const actionTypes = require('../action-types/action-types');
 const initState = {
   cartItems: [],
-  cartLocation: {}
+  showCartCalculations: false,
+  showLocationAndTime: false,
+  locationAndTime: {}
 };
 const addCartItemHandler = (state, item) => {
   const existingItemIndex = state.cartItems.findIndex(cartItem => cartItem.index === item.index);
   if(existingItemIndex > -1){
     const newCartItems = [...state.cartItems];
     newCartItems[existingItemIndex].quantity += item.quantity;
-    return {...state, cartItems: newCartItems};
+    return {...state, cartItems: newCartItems, showCartCalculations: true};
   }
-  return {...state, cartItems: state.cartItems.concat(item)};
+  return {...state, cartItems: state.cartItems.concat(item), showCartCalculations: true};
 }
 const removeCartItemHandler = (state, index) => {
   const arrIndex = state.cartItems.findIndex(item => item.index === index);
   const cartItems = state.cartItems.slice(0, arrIndex).concat(state.cartItems.slice(arrIndex + 1));
-  return {...state, cartItems};
+  if(cartItems.length < 1){
+    return {...state, cartItems, showCartCalculations: false};
+  } else {
+    return {...state, cartItems};
+  }
 }
-const updateLocationHandler = (state, location) => ({
+const updateLocationAndTimeHandler = (state, obj) => ({
   ...state,
-  cartLocation: location
+  showLocationAndTime: true,
+  locationAndTime: obj
 });
 const cartReducer = (state = initState, action) => {
   switch(action.type){
     case actionTypes.addCartItem: return addCartItemHandler(state, action.val);
     case actionTypes.removeCartItem: return removeCartItemHandler(state, action.val);
-    case actionTypes.updateLocation: return updateLocationHandler(state, action.val);
+    case actionTypes.updateLocationAndTime: return updateLocationAndTimeHandler(state, action.val);
     default: return state;
   }
 }
