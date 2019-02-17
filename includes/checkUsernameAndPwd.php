db07f5858e70dd1d4379ac4d50a935cd38810382
James Russo <1jlrusso@gmail.com>
2019-02-17 12:25:32 -0800

    reconfig username and pwd check as object instance

--- includes/checkUsernameAndPwd.php
+++ includes/checkUsernameAndPwd.php
@@ -1,30 +1,64 @@
 <?php
-session_start();
-if(isset($_POST["username"]) && isset($_POST["pwd"])){
-  include_once "dbh-inc.php";
-  $username = mysqli_real_escape_string($conn, $_POST["username"]);
-  $pwd = mysqli_real_escape_string($conn, $_POST["pwd"]);
-  $checkUsername = "SELECT * FROM users WHERE user_username=?;";
-  $stmt = mysqli_stmt_init($conn);
-  if(!mysqli_stmt_prepare($stmt, $checkUsername)){
-    header("Location: https://www.gogobin.com?stmt-error");
-    exit();
-  } else {
+include_once "dbh-inc.php";
+
+class UsernamePwdCheck {
+  private $returning_user = [];
+  public function __construct($username, $pwd){
+    $this->returning_user['username'] = $username;
+    $this->returning_user['password'] = $pwd;
+  }
+  public function process_user(){
+    foreach($this->returning_user as $part){
+      $isEmpty = $this->empty_val_check($part);
+      if($isEmpty){
+        $this->end_process('empty-field');
+        return;
+      }
+    }
+    $this->check_username_prep();
+  }
+  private function check_username_prep(){
+    $sql_check_username = "SELECT * FROM users WHERE user_username = ?";
+    $stmt = mysqli_stmt_init($GLOBALS['conn']);
+    $prep_valid = mysqli_stmt_prepare($stmt, $sql_check_username);
+    if(!$prep_valid){
+      $this->end_process('username-check-fail');
+    } else {
+      $this->check_username($stmt);
+    }
+  }
+  private function check_username($stmt){
+    $username = $this->returning_user['username'];
     mysqli_stmt_bind_param($stmt, "s", $username);
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
-    $resultRows = mysqli_num_rows($result);
-    if($resultRows > 0){
-      $resultData = mysqli_fetch_all($result, MYSQLI_ASSOC);
-      $pwdCheck = password_verify($pwd, $resultData[0]["user_pwd"]);
-      if(!$pwdCheck){
-        echo "incorrect";
-      }
+    $result_rows = mysqli_num_rows($result);
+    if($result_rows > 0){
+      $row = mysqli_fetch_assoc($result);
+      $this->verify_password($row);
     } else {
-      echo "incorrect";
+      $this->end_process('invalid-username');
     }
   }
-} else {
-  header("Location: https://www.gogobin.com");
-  exit();
+  private function verify_password($row){
+    $pwd = $this->returning_user['password'];
+    $pwd_valid = password_verify($pwd, $row['user_pwd']);
+    if(!$pwd_valid){
+      $this->end_process('invalid-pwd');
+    }
+  }
+  private function empty_val_check($part){
+    if(empty($part)){
+      return true;
+    }
+    return false;
+  }
+  private function end_process($error){
+    exit($error);
+  }
 }
+
+$username = mysqli_real_escape_string($GLOBALS['conn'], $_POST['username']);
+$pwd = mysqli_real_escape_string($GLOBALS['conn'], $_POST['pwd']);
+$ret_user = new UsernamePwdCheck($username, $pwd);
+$ret_user->process_user();
