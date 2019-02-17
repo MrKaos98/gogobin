f7783172403ad9463050717b4aa513938935a822
James Russo <1jlrusso@gmail.com>
2019-02-17 12:20:12 -0800

    add new action types to edit cart items, check user auth, and reset cart

--- store/action-types/action-types.js
+++ store/action-types/action-types.js
@@ -4,7 +4,11 @@ const completeOrder = 'COMPLETE_ORDER';
 const checkCurrentOrders = 'CHECK_CURRENT_ORDERS';
 const addCartItem = 'ADD_CART_ITEM';
 const removeCartItem = 'REMOVE_CART_ITEM';
+const editCartItem = 'EDIT_CART_ITEM';
+const resetOpenSpecificItem = 'RESET_OPEN_SPECIFIC_ITEM';
 const updateLocationAndTime = 'UPDATE_CART_LOCATION';
+const userAuth = 'USER_AUTH';
+const resetCart = 'RESET_CART';
 
 module.exports = {
   toggleLogin,
@@ -13,5 +17,9 @@ module.exports = {
   checkCurrentOrders,
   addCartItem,
   removeCartItem,
-  updateLocationAndTime
+  editCartItem,
+  updateLocationAndTime,
+  resetOpenSpecificItem,
+  userAuth,
+  resetCart
 };
