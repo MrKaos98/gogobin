4bff1da9df44b88513b54c2a554220b8cf7f1081
James Russo <1jlrusso@gmail.com>
2019-02-17 12:17:00 -0800

    pass auth reducer to combineReducers

--- store/store-index.js
+++ store/store-index.js
@@ -2,7 +2,9 @@ const redux = require('redux');
 const loginReducer = require('./reducers/login-reducer');
 const orderReducer = require('./reducers/order-reducer');
 const cartReducer = require('./reducers/cart-reducer');
+const authReducer = require('./reducers/auth-reducer');
 const rootReducer = redux.combineReducers({
+  auth: authReducer,
   login: loginReducer,
   order: orderReducer,
   cart: cartReducer
